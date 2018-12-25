2e96db8ad5d78a8f43684d2bc6baaa4ce64d5af8
mrzhangs1015916 <807167807@qq.com>
2018-12-25 10:50:06 +0800

    学习

---
+++ src/main/java/com/admxj/spring/SpringDemo/SpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.admxj.spring.SpringDemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/admxj/spring/SpringDemo/service/MailServiceTest.java
@@ -0,0 +1,85 @@
+package com.admxj.spring.SpringDemo.service;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+import javax.annotation.Resource;
+import javax.mail.MessagingException;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by admxj on 2018/10/6.
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MailServiceTest {
+
+    @Autowired
+    private MailService mailService;
+
+    @Resource
+    private TemplateEngine templateEngine;
+
+    @Test
+    public void sendSimpleMail() {
+        mailService.sendSimpleMail("807167807@qq.com","测试spring boot mail","测试spring boot mial 内容");
+    }
+
+    @Test
+    public void sendHtmlMail() throws MessagingException {
+
+        String content = "<html>\n" +
+                "<body>\n" +
+                "<h3>hello world</h3>\n" +
+                "<h1>html</h1>\n" +
+                "<body>\n" +
+                "</html>\n";
+
+        mailService.sendHtmlMail("807167807@qq.com","这是一封HTML邮件",content);
+    }
+
+    @Test
+    public void sendAttachmentsMail() throws MessagingException {
+        String filePath = "src/main/resources/data/error.xlsx";
+        String content = "<html>\n" +
+                "<body>\n" +
+                "<h3>hello world</h3>\n" +
+                "<h1>html</h1>\n" +
+                "<h1>附件传输</h1>\n" +
+                "<body>\n" +
+                "</html>\n";
+        mailService.sendAttachmentsMail("807167807@qq.com","这是一封HTML邮件",content, filePath);
+    }
+
+    @Test
+    public void sendInlinkResourceMail() throws MessagingException {
+        String imgPath = "src/main/resources/data/bug.JPG";
+        String rscId = "admxj001";
+        String content = "<html>" +
+                "<body>" +
+                "<h3>hello world</h3>" +
+                "<h1>html</h1>" +
+                "<h1>图片邮件</h1>" +
+                "<img src='cid:"+rscId+"'></img>" +
+                "<body>" +
+                "</html>";
+
+        mailService.sendInlinkResourceMail("807167807@qq.com","这是一封图片邮件",content, imgPath, rscId);
+    }
+
+    @Test
+    public void testTemplateMailTest() throws MessagingException {
+        Context context = new Context();
+        context.setVariable("id","0096");
+
+        String emailContent = templateEngine.process("emailTeplate", context);
+        mailService.sendHtmlMail("807167807@qq.com","这是一封HTML邮件",emailContent);
+
+    }
+}
